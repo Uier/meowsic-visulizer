0cf5cdea8db581be68ac7655cfccbe49a4a488cb
Uier <ap9940506@gmail.com>
2020-01-09 05:20:39 +0800

    meow meow

--- cube3_array_music.js
+++ cube3_array_music.js
@@ -10,8 +10,9 @@ var musicStarted = 0;
 var pointsArray = [];
 var volumeArray = [];
 var texCoordsArray = [];
+var texArray = [];
 
-var texture;
+var textures = [];
 
 var texCoord = [
     vec2(0, 1),
@@ -21,50 +22,71 @@ var texCoord = [
 ];
 
 // place one square into pointsArray
-function square(midX, midY, width, height) {
+function square(midX, midY, width, height, tex) {
     let delX = width / 2;
     let delY = height / 2;
     pointsArray.push(vec2(midX - delX, midY + delY)); // left up
-    // console.log(midX - delX, midY + delY); // left up
     texCoordsArray.push(texCoord[0]);
+    texArray.push(tex);
     pointsArray.push(vec2(midX + delX, midY + delY)); // right up
-    // console.log(midX + delX, midY + delY); // right up
     texCoordsArray.push(texCoord[1]);
+    texArray.push(tex);
     pointsArray.push(vec2(midX - delX, midY - delY)); // left down
-    // console.log(midX - delX, midY - delY); // left down
     texCoordsArray.push(texCoord[2]);
+    texArray.push(tex);
     pointsArray.push(vec2(midX + delX, midY + delY)); // right up
-    // console.log(midX + delX, midY + delY); // right up
     texCoordsArray.push(texCoord[1]);
+    texArray.push(tex);
     pointsArray.push(vec2(midX - delX, midY - delY)); // left down
-    // console.log(midX - delX, midY - delY); // left down
     texCoordsArray.push(texCoord[2]);
+    texArray.push(tex);
     pointsArray.push(vec2(midX + delX, midY - delY)); // right down
-    console.log(midX, midY); // right down
     texCoordsArray.push(texCoord[3]);
+    texArray.push(tex);
 }
+
+var meowDiff = 2 / (N * 6 + 4) * 2;
 
 function colorCube() {
     let padding = 2 / (N * 6 + 4);
     let width = padding * 2;
     let delta = width + padding;
     let leftBorder = -1+padding+(width/2);
-    for ( let i=0; i<numVertices; i++ )
-        square( leftBorder+i*delta, 0, width, 0.5 );
+    for ( let i=0; i<numVertices; i++ ) {
+        // bar
+        square( leftBorder+i*delta, 0, width, 0.4, 1 );
+        // meow
+        square( leftBorder+i*delta, 0.4, width, width, 0);
+    }
+}
+
+function configureTexture() {
+    var texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    // gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, false);
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+    return texture;
 }
 
 // set a image to texture buffer
-// configureImage(src, n) {
-//     var image = new Image();
-//     image.onload = function () {
-//         gl.activeTexture(gl.TEXTURE0 + n);
-//         gl.bindTexture(gl.TEXTURE_2D, this.textures[n]);
-//         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-//     }.bind(this);
-//     image.src = src;
+function configureImage(src, n) {
+    var image = new Image();
+    image.onload = function () {
+        gl.activeTexture(gl.TEXTURE0 + n);
+        gl.bindTexture(gl.TEXTURE_2D, textures[n]);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    }
+    image.src = src;
 
-//     return image;
-// }
+    return image;
+}
 
 var analyser;
 var frequencyData = new Uint8Array();
@@ -86,10 +108,26 @@ window.onload = function init() {
     program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
-	// Generate pointsArray[]
-	// We don't use indices and ELEMENT_ARRAY_BUFFER (as in previous example)
-	// because we need to assign different face normals to shared vertices.
-	colorCube();
+    // setup textures meow.png
+    textures.push(this.configureTexture());
+    configureImage('image/meow.png', 0);
+
+    textures.push(this.configureTexture());
+    configureImage('image/blank.png', 1);
+
+    gl.uniform1iv(gl.getUniformLocation(program, 'u_texture'), [0, 1]);
+
+    //event listeners for buttons 
+    document.getElementById( "myBtn" ).onclick = startMusic;
+
+    render();
+};
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+
+    // setup bars
+    colorCube();
     
     // vertex array attribute buffer
     var vBuffer = gl.createBuffer();
@@ -109,32 +147,43 @@ window.onload = function init() {
     gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vTexCoord );
 
-    // setup textures (cat body, cat head)
-    // configureImage('image/cat-body.png', 0);
-    // configureImage('image/cat-head.png', 1);
-
-    //event listeners for buttons 
-    document.getElementById( "myBtn" ).onclick = startMusic;
-
-    render();
-};
-
-function render() {
-    gl.clear( gl.COLOR_BUFFER_BIT );
+    // tex attribute
+    var ttBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, ttBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(texArray), gl.STATIC_DRAW );
+    
+    var vTexIdx = gl.getAttribLocation( program, "vTexIdx" );
+    gl.vertexAttribPointer( vTexIdx, 1, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vTexIdx );
 
 	// update data in frequencyData
     if (musicStarted) analyser.getByteFrequencyData(frequencyData);
-
+    
     // render frame based on values in frequencyData
     if (musicStarted)  {
         volumeArray = [];
+        // bar: 3, 5, 6 wont move
+        // meow: all move
         for ( let i=-N; i<=N; i++ ) {
-            volumeArray.push(frequencyData[Math.floor(256/numVertices)*(i+N)] / 255 * N );
-            volumeArray.push(frequencyData[Math.floor(256/numVertices)*(i+N)] / 255 * N );
-            volumeArray.push(10);
-            volumeArray.push(frequencyData[Math.floor(256/numVertices)*(i+N)] / 255 * N );
-            volumeArray.push(10);
-            volumeArray.push(10);
+            let volumeValue = frequencyData[Math.floor(256/numVertices)*(i+N)] / 255 * N;
+            volumeValue = Math.pow(volumeValue, 4)*0.001;
+            for ( let j=1; j<=12; j++ ) {
+                if ( j == 3 || j == 5 || j == 6 ) {
+                    volumeArray.push(1);
+                } else if ( j == 7 || j == 8 || j == 10 ) {
+                    if ( volumeValue == 0 ) {
+                        volumeArray.push(1);
+                    } else {
+                        volumeArray.push(volumeValue*((meowDiff/volumeValue+0.4-meowDiff)/0.4+meowDiff))
+                    }
+                } else {
+                    if ( volumeValue == 0 ) {
+                        volumeArray.push(1);
+                    } else {
+                        volumeArray.push(volumeValue);
+                    }
+                }
+            }
         }
 
         var vBuffer = gl.createBuffer();
@@ -145,7 +194,11 @@ function render() {
         gl.vertexAttribPointer( vVolume, 1, gl.FLOAT, false, 0, 0 );
         gl.enableVertexAttribArray( vVolume );
     }
-    gl.drawArrays( gl.TRIANGLES, 0, numVertices*6 );
+    gl.drawArrays( gl.TRIANGLES, 0, numVertices*12 );
+
+    pointsArray = [];
+    texCoordsArray = [];
+    texArray = [];
 
     requestAnimFrame( render );
 }
